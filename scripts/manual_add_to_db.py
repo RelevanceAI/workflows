05d91550f25552be1f8e64b1303445d30c2113b2
jacky2wong <jacky2wong@gmail.com>
2022-03-10 15:48:29 +1100

    fix workflow

--- scripts/manual_add_to_db.py
+++ scripts/manual_add_to_db.py
@@ -12,7 +12,7 @@ from relevanceai import Client
 client = Client(token=os.getenv("SUPPORT_ACTIVATION_TOKEN"), force_refresh=True)
 
 # Workflows data
-ds = client.Dataset("workflows-data")
+ds = client.Dataset("recipes-and-workflows")
 # ds.delete()
 
 # Recipes data
