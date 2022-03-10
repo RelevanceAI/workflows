796d38df8a87ef4d966b662071b4de919665c505
ofrighil <eugene.ofriel@gmail.com>
2022-03-10 11:58:28 -0500

    change structure to accomodate tokens

--- scripts/test_notebooks.py
+++ scripts/test_notebooks.py
@@ -1,3 +1,5 @@
+import base64
+import json
 import logging
 import os
 import re
@@ -15,22 +17,9 @@ WORKFLOWS = Path(__file__).resolve().parent.parent / Path("workflows")
 
 logging.basicConfig(level=logging.INFO)
 
-Credentials = namedtuple("Credentials", ["project", "api_key", "region"])
-
-def get_credentials(region: str) -> Credentials:
-    """
-    Get credentials required to initialize Relevance AI client.
-    """
-    if region == "us-east-1":
-        project = os.getenv("TEST_US_PROJECT")
-        api_key = os.getenv("TEST_US_API_KEY")
-    elif region == "old-australia-east":
-        project = os.getenv("TEST_PROJECT")
-        api_key = os.getenv("TEST_API_KEY")
-    else:
-        raise ValueError("Invalid region")
-    
-    return Credentials(project, api_key, region)
+Credentials = namedtuple(
+    "Credentials", ["project", "api_key", "region", "token"]
+)
 
 def get_paths() -> List[Path]:
     """
@@ -73,22 +62,75 @@ def get_paths() -> List[Path]:
     
     return paths
 
+def get_credentials(path: Path, tokens: dict, region: str) -> Credentials:
+    """
+    Get credentials required to initialize Relevance AI client.
+    """
+    token = tokens.get("WORKFLOW_TOKEN_" + path.stem.upper())
+    if token is not None:
+        preconfiguration = json.loads(base64.b64decode(token + "==="))
+        project = preconfiguration["project"]
+        api_key = preconfiguration["api_key"]
+        region = preconfiguration["region"]
+    else:
+        if region == "us-east-1":
+            project = os.getenv("TEST_US_PROJECT")
+            api_key = os.getenv("TEST_US_API_KEY")
+        elif region == "old-australia-east":
+            project = os.getenv("TEST_PROJECT")
+            api_key = os.getenv("TEST_API_KEY")
+        else:
+            raise ValueError("Invalid region")
+    
+    return Credentials(project, api_key, region, token)
+
+def get_all_credentials(paths: List[Path], region: str) -> List[Credentials]:
+    tokens = { 
+        token: os.environ[token]
+        for token in filter(
+            lambda var: var.startswith("WORKFLOW_TOKEN"),
+            os.environ
+        )
+    }
+    return [ get_credentials(path, tokens, region) for path in paths ]
+
 def insert_credentials(
     notebook: dict, credentials: Credentials
 ) -> None:
     """
     Modifies a notebook (formatted as a dictionary) so that the Relevance AI
-    client has the proper credentials.
+    client has the proper credentials. In the event that the notebook is
+    activated by a token, credentials will not be added.
     """
     for cell in notebook["cells"]:
+        # Simultaneously check for both the token and client works correctly
+        # because the token must be defined before client in the notebook. A
+        # sufficient condition to execute the notebook when exactly one of 
+        # these are filled.
         if cell["cell_type"] == "code":
             source = cell["source"]
-            regex_result = re.search("Client\(.*\)", source)
-            if regex_result is not None:
-                start, end = regex_result.span()
+
+            token_regex = re.search(
+                "token\s*=\s*((\".*\")|(\'.*\'))",
+                source
+            )
+            if token_regex is not None:
+                start, end = token_regex.span()
+                cell["source"] = "".join([
+                    source[:start],
+                    token_regex.group(1),
+                    credentials.token,
+                    token_regex.group(2),
+                    source[end:]
+                ])
+                break # If a token exists, no need to go further
+
+            client_regex = re.search("Client\(.*\)", source)
+            if client_regex is not None:
+                start, end = client_regex.span()
                 # comma at the end is in case Client has existing arguments
                 cell["source"] = "".join([
-                    source[:start+6+1], # 'Client' has length 6
+                    source[:start+6+1], # 'Client(' has length 6 + 1
                     "project=\"{}\",api_key=\"{}\",region=\"{}\",".format(
                         credentials.project,
                         credentials.api_key,
@@ -112,7 +154,7 @@ def insert_name(notebook: dict, path: Path) -> dict:
 
 def generate_notebooks(
     paths: List[Path],
-    credentials: Credentials
+    all_credentials: List[Credentials]
 ) -> List[dict]:
     """
     Map each path into a notebook (formatted as a dictionary) and ensure that
@@ -126,16 +168,18 @@ def generate_notebooks(
                 credentials
             ),
             path
-        ) for path in paths
+        ) for path, credentials in zip(paths, all_credentials)
     ]
 
-def get_notebooks(credentials: Credentials) -> List[dict]:
+def get_notebooks(
+    paths: List[Path], all_credentials: List[Credentials]
+) -> List[dict]:
     """
     Gets and sets all notebooks to be checked.
     """
     return generate_notebooks(
-        get_paths(),
-        credentials
+        paths,
+        all_credentials
     )
 
 def execute_notebook(notebook: dict) -> dict:
@@ -148,7 +192,7 @@ def execute_notebook(notebook: dict) -> dict:
         client = NotebookClient(
             notebook,
             timeout=600,
-            kernel_name="python3"
+            kernel_name="relevanceai"
         )
         client.execute()
         return {}
@@ -182,6 +226,11 @@ def mask_credentials(result: dict, credentials: Credentials) -> None:
         credentials.api_key,
         "*" * len(credentials.api_key)
     )
+    if credentials.token is not None:
+        error = error.replace(
+            credentials.token,
+            "*" * len(credentials.token)
+        )
 
     result["error"].traceback = error
 
@@ -197,14 +246,16 @@ def print_error(result: dict, credentials: Credentials) -> None:
         print(result["error"])
         print(error_footer)
 
-def print_errors(results: List[dict], credentials: Credentials) -> None:
+def print_errors(
+    results: List[dict], all_credentials: List[Credentials]
+) -> None:
     """
     Print the errors of multiple results, if the results contain errors, and
     raise an error at the end (again, if there exists any errors).
     """
     if any(results):
         # If there are any non-empty results, that means an error occurred
-        for result in results:
+        for result, credentials in zip(results, all_credentials):
             print_error(result, credentials)
         else:
             raise Exception("At least one of the notebook checks failed.")
@@ -213,7 +264,20 @@ def print_errors(results: List[dict], credentials: Credentials) -> None:
 
 if __name__ == "__main__":
     region = "us-east-1"
-    credentials = get_credentials(region)
-    notebooks = get_notebooks(credentials)
+    paths = get_paths()
+    all_credentials = get_all_credentials(paths, region)
+    notebooks = get_notebooks(paths, all_credentials)
+
+    tokens = { 
+        token: os.environ[token]
+        for token in filter(lambda var: var.startswith("WORKFLOW_TOKEN"), os.environ)
+    }
+
+    #print(get_credentials(notebook_paths[0], tokens, region))
+    #val = tokens.get("WORKFLOW_TOKEN_VECTORIZE_YOUR_DATA_WITH_RELEVANCE_AI")
+    #print(val)
+    #print(val)
+    #notebook_credentials = get_credentials(notebook_paths, region)
+    #credentials = get_credentials(region)
     results = execute_notebooks(notebooks)
-    print_errors(results, credentials)
+    print_errors(results, all_credentials)
