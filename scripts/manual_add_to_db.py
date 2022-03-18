39d2b8715b2c49d14dbee17a22239c8d721a94a3
boba_and_beer <jacky2wong@gmail.com>
2022-03-18 11:27:25 +1100

    Update manual_add_to_db.py

--- scripts/manual_add_to_db.py
+++ scripts/manual_add_to_db.py
@@ -259,7 +259,8 @@ WORKFLOWS = [
             "recipe": True, # Required for recipes
             "feature_image_url": "https://relevance.ai/wp-content/uploads/2022/03/image-6.png",
             "blog_link": "https://relevance.ai/twitter-data-workflow-how-to-run-twitter-account-data-analysis/",
-            "recipe_url": "https://relevance.ai"
+            "recipe_url": "https://relevance.ai",
+            "logo_url": "https://www.svgrepo.com/show/22159/twitter.svg",
         },
     ]
 
