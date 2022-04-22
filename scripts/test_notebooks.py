4722c774c6e6935e0a8273742a5b8c3b8c049cc4
Joseph Twin <joseph.twin@relevance.ai>
2022-04-22 15:13:18 +1000

    black

--- scripts/test_notebooks.py
+++ scripts/test_notebooks.py
@@ -17,9 +17,8 @@ WORKFLOWS = Path(__file__).resolve().parent.parent / Path("workflows")
 
 logging.basicConfig(level=logging.INFO)
 
-Credentials = namedtuple(
-    "Credentials", ["project", "api_key", "region", "token"]
-)
+Credentials = namedtuple("Credentials", ["project", "api_key", "region", "token"])
+
 
 def get_paths() -> List[Path]:
     """
@@ -29,38 +28,32 @@ def get_paths() -> List[Path]:
         ignored_notebooks = set(
             WORKFLOWS / Path(notebook.strip())
             for notebook in file.readlines()
-            if not notebook.startswith('#')
+            if not notebook.startswith("#")
         )
     logging.info(
-        "Ignoring the following notebooks: " +
-        "".join(map(
-            lambda notebook: f"\n  * {str(notebook)}",
-            ignored_notebooks
-        ))
+        "Ignoring the following notebooks: "
+        + "".join(map(lambda notebook: f"\n  * {str(notebook)}", ignored_notebooks))
     )
 
     workflows = [
-        workflow_dir for workflow_dir in WORKFLOWS.iterdir()
-        if workflow_dir.is_dir()
+        workflow_dir for workflow_dir in WORKFLOWS.iterdir() if workflow_dir.is_dir()
     ]
     paths = []
     for workflow in workflows:
-        paths.extend([
-            notebook for notebook in workflow.iterdir()
-            if (
-                notebook.suffix == ".ipynb" and
-                notebook not in ignored_notebooks
-            )
-        ])
+        paths.extend(
+            [
+                notebook
+                for notebook in workflow.iterdir()
+                if (notebook.suffix == ".ipynb" and notebook not in ignored_notebooks)
+            ]
+        )
     logging.info(
-        "Checking the following notebooks: " +
-        "".join(map(
-            lambda path: f"\n  * {str(path)}",
-            paths 
-        ))
+        "Checking the following notebooks: "
+        + "".join(map(lambda path: f"\n  * {str(path)}", paths))
     )
-    
+
     return paths
+
 
 def get_credentials(path: Path, tokens: dict, region: str) -> Credentials:
     """
@@ -81,8 +74,9 @@ def get_credentials(path: Path, tokens: dict, region: str) -> Credentials:
             api_key = os.getenv("TEST_API_KEY")
         else:
             raise ValueError("Invalid region")
-    
+
     return Credentials(project, api_key, region, token)
+
 
 def get_all_credentials(paths: List[Path], region: str) -> List[Credentials]:
     """
@@ -90,18 +84,14 @@ def get_all_credentials(paths: List[Path], region: str) -> List[Credentials]:
     merits its own credentials because it may be different if it uses a
     token.
     """
-    tokens = { 
+    tokens = {
         token: os.environ[token]
-        for token in filter(
-            lambda var: var.startswith("WORKFLOW_TOKEN"),
-            os.environ
-        )
+        for token in filter(lambda var: var.startswith("WORKFLOW_TOKEN"), os.environ)
     }
-    return [ get_credentials(path, tokens, region) for path in paths ]
+    return [get_credentials(path, tokens, region) for path in paths]
 
-def insert_credentials(
-    notebook: dict, credentials: Credentials
-) -> None:
+
+def insert_credentials(notebook: dict, credentials: Credentials) -> None:
     """
     Modifies a notebook (formatted as a dictionary) so that the Relevance AI
     client has the proper credentials. In the event that the notebook is
@@ -110,45 +100,48 @@ def insert_credentials(
     for cell in notebook["cells"]:
         # Simultaneously check for both the token and client works correctly
         # because the token must be defined before client in the notebook. A
-        # sufficient condition to execute the notebook when exactly one of 
+        # sufficient condition to execute the notebook when exactly one of
         # these are filled.
         if cell["cell_type"] == "code":
             source = cell["source"]
 
             token_regex = re.search(
-                "(token\s*=\s*(\"|\'))(.*?(\"|\'))",
-                #"token\s*=\s*((\".*?\")|(\'.*?\'))",
-                source
+                "(token\s*=\s*(\"|'))(.*?(\"|'))",
+                # "token\s*=\s*((\".*?\")|(\'.*?\'))",
+                source,
             )
             if token_regex is not None:
                 start, end = token_regex.span()
-                cell["source"] = "".join([
-                    source[:start],
-                    token_regex.group(1),
-                    credentials.token,
-                    token_regex.group(2)[-1], # add back the '"'
-                    source[end:]
-                ])
-                break # If a token exists, no need to go further
+                cell["source"] = "".join(
+                    [
+                        source[:start],
+                        token_regex.group(1),
+                        credentials.token,
+                        token_regex.group(2)[-1],  # add back the '"'
+                        source[end:],
+                    ]
+                )
+                break  # If a token exists, no need to go further
 
             client_regex = re.search("Client\(.*\)", source)
             if client_regex is not None:
                 start, end = client_regex.span()
                 # comma at the end is in case Client has existing arguments
-                cell["source"] = "".join([
-                    source[:start+6+1], # 'Client(' has length 6 + 1
-                    "project=\"{}\",api_key=\"{}\",region=\"{}\",".format(
-                        credentials.project,
-                        credentials.api_key,
-                        credentials.region
-                    ),
-                    source[start+6+1:] 
-                ])
-                break # No need to continue after inserting credentials
+                cell["source"] = "".join(
+                    [
+                        source[: start + 6 + 1],  # 'Client(' has length 6 + 1
+                        'project="{}",api_key="{}",region="{}",'.format(
+                            credentials.project, credentials.api_key, credentials.region
+                        ),
+                        source[start + 6 + 1 :],
+                    ]
+                )
+                break  # No need to continue after inserting credentials
         else:
             continue
-    
+
     return notebook
+
 
 def insert_name(notebook: dict, path: Path) -> dict:
     """
@@ -158,9 +151,9 @@ def insert_name(notebook: dict, path: Path) -> dict:
     notebook["metadata"]["temporary_name"] = name
     return notebook
 
+
 def generate_notebooks(
-    paths: List[Path],
-    all_credentials: List[Credentials]
+    paths: List[Path], all_credentials: List[Credentials]
 ) -> List[dict]:
     """
     Map each path into a notebook (formatted as a dictionary) and ensure that
@@ -169,24 +162,19 @@ def generate_notebooks(
     """
     return [
         insert_name(
-            insert_credentials(
-                nbformat.read(path, nbformat.NO_CONVERT),
-                credentials
-            ),
-            path
-        ) for path, credentials in zip(paths, all_credentials)
+            insert_credentials(nbformat.read(path, nbformat.NO_CONVERT), credentials),
+            path,
+        )
+        for path, credentials in zip(paths, all_credentials)
     ]
 
-def get_notebooks(
-    paths: List[Path], all_credentials: List[Credentials]
-) -> List[dict]:
+
+def get_notebooks(paths: List[Path], all_credentials: List[Credentials]) -> List[dict]:
     """
     Gets and sets all notebooks to be checked.
     """
-    return generate_notebooks(
-        paths,
-        all_credentials
-    )
+    return generate_notebooks(paths, all_credentials)
+
 
 def execute_notebook(notebook: dict) -> dict:
     """
@@ -195,15 +183,12 @@ def execute_notebook(notebook: dict) -> dict:
     notebook_name = notebook["metadata"]["temporary_name"]
     logging.info(f"Checking {notebook_name}")
     try:
-        client = NotebookClient(
-            notebook,
-            timeout=600,
-            kernel_name="python3"
-        )
+        client = NotebookClient(notebook, timeout=600, kernel_name="python3")
         client.execute()
         return {}
     except Exception as err:
         return {"notebook": notebook_name, "error": err}
+
 
 def execute_notebooks(
     notebooks: List[dict],
@@ -211,10 +196,11 @@ def execute_notebooks(
     chunksize: Optional[int] = 1,
 ):
     """
-    Executes multiple notebooks in parallel. 
+    Executes multiple notebooks in parallel.
     """
     with Pool(processes=processes) as pool:
         return pool.map(execute_notebook, notebooks, chunksize=chunksize)
+
 
 def mask_credentials(result: dict, credentials: Credentials) -> None:
     """
@@ -224,21 +210,13 @@ def mask_credentials(result: dict, credentials: Credentials) -> None:
 
     # Preemptively ensure that the project and API key are masked in the
     # error message, if it shows up at all.
-    error = error.replace(
-        credentials.project,
-        "*" * len(credentials.project)
-    )
-    error = error.replace(
-        credentials.api_key,
-        "*" * len(credentials.api_key)
-    )
+    error = error.replace(credentials.project, "*" * len(credentials.project))
+    error = error.replace(credentials.api_key, "*" * len(credentials.api_key))
     if credentials.token is not None:
-        error = error.replace(
-            credentials.token,
-            "*" * len(credentials.token)
-        )
+        error = error.replace(credentials.token, "*" * len(credentials.token))
 
     result["error"].traceback = error
+
 
 def print_error(result: dict, credentials: Credentials) -> None:
     """
@@ -252,9 +230,8 @@ def print_error(result: dict, credentials: Credentials) -> None:
         print(result["error"])
         print(error_footer)
 
-def print_errors(
-    results: List[dict], all_credentials: List[Credentials]
-) -> None:
+
+def print_errors(results: List[dict], all_credentials: List[Credentials]) -> None:
     """
     Print the errors of multiple results, if the results contain errors, and
     raise an error at the end (again, if there exists any errors).
@@ -267,6 +244,7 @@ def print_errors(
             raise Exception("At least one of the notebook checks failed.")
     else:
         print("No errors occurred while checking notebooks.")
+
 
 if __name__ == "__main__":
     region = "us-east-1"
